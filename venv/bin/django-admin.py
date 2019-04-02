3d0856c7d54931da8cb40ff14114beb98f6a389e
cnaver1 <cnaver1@lsu.edu>
2019-04-01 21:11:10 -0500

    Django Heroku App
    ISDS 3107 Django Heroku App

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cameronnavero/ISDS_3107/mysite/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ venv/lib/python3.7/__future__.py
@@ -0,0 +1,1 @@
+/Users/cameronnavero/anaconda3/lib/python3.7/__future__.py
